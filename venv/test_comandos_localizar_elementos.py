3d7c673bfc3f412924319fbd9f6d9e023e2f2546
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-08-14 13:43:34 -0300

    Aula 17

---
+++ venv/test_comandos_localizar_elementos.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome() 
+
+browser.maximize_window()
+browser.get("https://www.saucedemo.com/")
+
+# username = browser.find_element(By.ID, "user-name")
+# password = browser.find_element(By.ID, "password")
+
+# username.send_keys('standad_user')
+# password.send_keys('secret_sauce')
+
+auth_fields = browser.find_elements(By.XPATH, "//*[@class='input_error form_input']")
+print(auth_fields)
+print(len(auth_fields))
+assert len(auth_fields) == 2, "Numero diferente do esperado"
+
+time.sleep(3)
+
+browser.quit()
