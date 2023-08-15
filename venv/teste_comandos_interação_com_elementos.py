a13589585575d3e085e8d164723cdb2995559f8e
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-08-15 13:43:43 -0300

    Meio Aula 19

---
+++ venv/teste_comandos_interação_com_elementos.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome() 
+
+browser.maximize_window()
+browser.get("https://www.saucedemo.com/")
+
+username = browser.find_element(By.ID, "user-name")
+password = browser.find_element(By.ID, "password")
+btn_login = browser.find_element(By.ID, 'login-button')
+
+username.send_keys('standard_user')
+password.send_keys('secret_sauce')
+
+btn_login.click()
+
+products_title = browser.find_element(By.XPATH, "//span[@class='title']")
+print(products_title.text)
+assert products_title.text == 'Products'
