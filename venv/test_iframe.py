a6d260de1e2f69218125122a77a5812998e1fd5d
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-08-21 13:46:27 -0300

    Atl

--- venv/test_iframe.py
+++ venv/test_iframe.py
@@ -1,13 +1,19 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+browser = webdriver.Chrome()
 browser.implicitly_wait(12)
                            
 
 browser.maximize_window()
-browser.get("https://chercher.tech/practice/practice-dropdowns-selenium-webdriver")
+browser.get("https://chercher.tech/practice/frames")
+
+
+iframe1 = browser.find_element(By.ID, "iframe1")
+browser.switch_to.frame(iframe1)
+browser.find_element(By.XPATH, "//*[@id='topic']/following-sibling::input").send_keys("iframe1")
+time.sleep(2)
